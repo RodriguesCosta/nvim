aeeff23cd5dbed25d4a760976e3ce988f21ab55f
Jose Rodrigues da Costa <jose@rodriguescosta.com>
2024-08-30 08:45:24 -0300

    feat: change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,34 @@
 return {
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     require("github-theme").setup({
+  --       -- ...
+  --     })
+  --
+  --     vim.cmd("colorscheme github_dark_default")
+  --   end,
+  -- },
+  -- {
+  --   "sainnhe/sonokai",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     -- Optionally configure and load the colorscheme
+  --     -- directly inside the plugin declaration.
+  --     vim.g.sonokai_enable_italic = true
+  --     vim.cmd.colorscheme("sonokai")
+  --     vim.g.sonokai_style = "andromeda"
+  --   end,
+  -- },
   {
-    "projekt0n/github-nvim-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "Mofiqul/dracula.nvim",
+    lazy = false,
+    priority = 1000,
     config = function()
-      require("github-theme").setup({
-        -- ...
-      })
-
-      vim.cmd("colorscheme github_dark_default")
+      vim.cmd.colorscheme("dracula")
     end,
   },
 }
