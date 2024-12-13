3d06e5eb75233b59ffad5d337363b2027b3c1b30
Jose Rodrigues da Costa <jose@rodriguescosta.com>
2024-12-13 11:11:23 -0300

    feat: update nvim and config

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -10,7 +10,7 @@ return {
         disable_auto_comment = true,
         accept_keymap = "<C-e>",
         dismiss_keymap = "<A-c>",
-        debounce_ms = 800,
+        debounce_ms = 1200,
         suggestion_color = { gui = "#808080", cterm = 244 },
         exclude_filetypes = { "TelescopePrompt", "NvimTree" },
         ignore_certificate_errors = false,
