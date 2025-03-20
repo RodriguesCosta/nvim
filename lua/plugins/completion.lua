d3ed17e8deca45fdecfdbca18a1838816233bf73
Jose Rodrigues <jose@skrip.dev>
2025-03-20 12:07:44 -0300

    refator: remove biome

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,20 +1,24 @@
 return {
   {
-    "codota/tabnine-nvim",
-    event = "VeryLazy",
-    build = "./dl_binaries.sh",
-    enabled = true,
-    cmd = { "TabnineStatus", "TabnineDisable", "TabnineEnable", "TabnineToggle" },
-    config = function()
-      require("tabnine").setup({
-        disable_auto_comment = true,
-        accept_keymap = "<C-e>",
-        dismiss_keymap = "<A-c>",
-        debounce_ms = 1200,
-        suggestion_color = { gui = "#808080", cterm = 244 },
-        exclude_filetypes = { "TelescopePrompt", "NvimTree" },
-        ignore_certificate_errors = false,
-      })
-    end,
+    "github/copilot.vim",
+    lazy = false,
   },
+  -- {
+  --   "codota/tabnine-nvim",
+  --   event = "VeryLazy",
+  --   build = "./dl_binaries.sh",
+  --   enabled = true,
+  --   cmd = { "TabnineStatus", "TabnineDisable", "TabnineEnable", "TabnineToggle" },
+  --   config = function()
+  --     require("tabnine").setup({
+  --       disable_auto_comment = true,
+  --       accept_keymap = "<C-e>",
+  --       dismiss_keymap = "<A-c>",
+  --       debounce_ms = 1200,
+  --       suggestion_color = { gui = "#808080", cterm = 244 },
+  --       exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+  --       ignore_certificate_errors = false,
+  --     })
+  --   end,
+  -- },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,46 +14,9 @@ return {
             "typescript-language-server",
             "tailwindcss-language-server",
             "prisma-language-server",
-            "biome",
           })
         end,
       },
     },
-    opts = function(_, opts)
-      local biome = { "biome-check", "biome" }
-
-      local user_opts = {
-        formatters_by_ft = {
-          json = biome,
-          jsonc = biome,
-          css = biome,
-          javascript = biome,
-          typescript = biome,
-          javascriptreact = biome,
-          typescriptreact = biome,
-        },
-        -- LazyVim will merge the options you set here with builtin formatters.
-        -- You can also define any custom formatters here.
-        ---@type table<string,table>
-        formatters = {
-          biome = {
-            cwd = require("conform.util").root_file({
-              "biome.json",
-              "biome.jsonc",
-            }),
-            require_cwd = true,
-          },
-          ["biome-check"] = {
-            cwd = require("conform.util").root_file({
-              "biome.json",
-              "biome.jsonc",
-            }),
-            require_cwd = true,
-          },
-        },
-      }
-
-      return vim.tbl_deep_extend("force", opts, user_opts)
-    end,
   },
 }
